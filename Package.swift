2e3769b6c6c45b5a5fa80ec58fa286a4421481fa
Aaron Ton <macos@192.168.30.104>
2024-05-02 13:33:41 +0800

    update

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HXPhotoPicker",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "HXPhotoPicker",
+            targets: ["HXPhotoPicker"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "HXPhotoPicker",
+            dependencies: ["Kingfisher"],
+            resources: [
+                .process("Resources")
+            ],
+            swiftSettings: [
+                .define("HXPICKER_ENABLE_SPM"),
+                .define("HXPICKER_ENABLE_PICKER"),
+                .define("HXPICKER_ENABLE_EDITOR"),
+                .define("HXPICKER_ENABLE_CAMERA")
+            ]),
+    ]
+)
